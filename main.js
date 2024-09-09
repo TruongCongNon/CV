146e3640ca11fd032e8f3066c6a78cae4e1663cf
TruongCongNon <truongcongnon104@gmail.com>
2024-09-09 14:59:28 +0700

    navbar-home-about-skills

---
+++ main.js
@@ -0,0 +1,16 @@
+
+//Loading 
+// const preloader = document.querySelector("[data-preaload]");
+// window.addEventListener("load", function () {
+
+//     preloader.classList.add("loaded");
+//     document.body.classList.add("loaded");
+// });
+// Typing animation
+const typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Freelancer",],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
