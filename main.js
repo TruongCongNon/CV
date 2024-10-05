c66dcb000863fa461cb2cdab403812310dccf0d8
TruongCongNon <truongcongnon104@gmail.com>
2024-10-05 15:00:46 +0700

    update: images

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.addEventListener("load", function () {
 });
 // Typing animation
 const typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Freelancer",],
+    strings: ["Frontend Developer", "Freelancer",""],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
